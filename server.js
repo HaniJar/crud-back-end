627ddd5418e638427ef64f01f2fb59f4df635eb9
Haniah Jardien <jardienhaniah@gmail.com>
2022-02-16 09:39:30 +0200

    first commit

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var corsOptions = {
   origin: "http://localhost:9001",
 };
 
-app.use(cors(corsOptions));
+app.use(cors());
 // parse requests of content-type - application/json
 
 app.use(express.json());
